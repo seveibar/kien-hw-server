75b80126467efceef62c8799efc9e785f9c92479
seveibar <seveibar@gmail.com>
2014-06-15 19:21:05 -0400

    clean argument and top level executable for setup

--- kien/setup/commandLineParser.py
+++ kien/setup/commandLineParser.py
@@ -6,24 +6,34 @@ import argparse
 # variables the user passed
 def parseSetupArguments(defaultConfigPath=None,defaultRemovePrevious=None):
 
-    parser = argparse.ArgumentParser(description='Perform setup operations for RPI Homework Server')
+    parser = argparse.ArgumentParser(description='Perform setup operations for Kien Homework Server')
 
     # --config argument
     parser.add_argument(
-        "--config","-c",
+        "--config", "-c",
         action="store",
         type=str,
         dest="configPath",
         help="Path to the setup \"config.json\" file. If left blank, this will default to the config.json with the directory \"setup.py\" is currently in.",
         default=defaultConfigPath)
 
-    # --config argument
+    # --remove-previous argument
     parser.add_argument(
-        "--remove-previous","-f",
+        "--remove-previous", "-f",
         dest="removePrevious",
         action="store_const",
         help="Remove previous data and site directories if they exist.",
         const=True
+        )
+
+    # --clean argument
+    parser.add_argument(
+        "--clean", "-l",
+        dest="clean",
+        action="store_const",
+        help="Remove any directories that were created as a result of a previous setup operation",
+        const=True,
+        default=False
         )
 
     # Parse command line arguments
